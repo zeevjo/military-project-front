e98356711db39a258f11eb4dff37cf905e6f39fb
Shahar Hayon <shaharhayon12300@gmail.com>
2025-01-12 17:52:34 +0200

    pre final login

--- login/login.js
+++ login/login.js
@@ -1,10 +1,11 @@
 const loginBTN = document.getElementById("loginBTN");
 const loginForm = document.getElementById("loginForm");
 const errorMessage = document.getElementById("errorMessage");
+const successMessage = document.getElementById("successMessage");
 
 loginBTN.addEventListener("click", function(event) {
     event.preventDefault();  
-    
+
     errorMessage.style.display = 'none';
     errorMessage.innerHTML = '';
     
@@ -21,12 +22,12 @@ loginBTN.addEventListener("click", function(event) {
         return;  
     }
 
-    // const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    // if (!emailPattern.test(userData.username)) {
-    //     errorMessage.style.display = 'block';
-    //     errorMessage.innerHTML = 'Please enter a valid email address.';
-    //     return;
-    // }
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailPattern.test(userData.username)) {
+        errorMessage.style.display = 'block';
+        errorMessage.innerHTML = 'Please enter a valid email address.';
+        return;
+    }
 
     fetch('http://localhost:8080/api/login', {
         method: 'POST',
@@ -37,8 +38,20 @@ loginBTN.addEventListener("click", function(event) {
     })
     .then(response => response.json())
     .then(data => {
-        console.log('Login successful:', data);
-        window.location.href = "/homepage/index.html"
+        if(data.success){
+            localStorage.setItem("username", userData.username);
+            localStorage.setItem("token", data.token);
+            
+            successMessage.style.display = 'block';
+            successMessage.innerHTML = data.message;
+            
+            setTimeout(()=>{
+                window.location.href = "/homepage/index.html"
+            }, 2000);
+        }else{
+            errorMessage.style.display = 'block';
+            errorMessage.innerHTML = data.message;
+        }
     })
     .catch(error => {
         errorMessage.style.display = 'block';
